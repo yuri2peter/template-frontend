ee95794703013e1c45ca031deece4956b4dd0587
yuri2peter <yuri2peter@qq.com>
2020-04-03 16:58:41 +0800

    reset config

--- config/publish.js
+++ config/publish.js
@@ -7,7 +7,6 @@ function getRandomPath() {
 
 module.exports = {
   // 发布文件的拷贝目录(默认产生一个随机目录，请手动指定，如path.resolve(__dirname, '../../your-build-repository')
-  path:
-    path.resolve(__dirname, '../../template-frontend-build') || getRandomPath(),
-  autoPush: true, // 是否自动push git仓库。若为true，则path需要为一个git仓库目录，程序将会自动拷贝文件后commit, push
+  path: '' || getRandomPath(),
+  autoPush: false, // 是否自动push git仓库。若为true，则path需要为一个git仓库目录，程序将会自动拷贝文件后commit, push
 };
