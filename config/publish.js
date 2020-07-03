61e61985fcc75a1cb4859183d4a0550f7452ca5a
yuri2 <yuri2peter@qq.com>
2020-07-03 19:27:54 +0800

    Update publish.js

--- config/publish.js
+++ config/publish.js
@@ -6,7 +6,7 @@ function getRandomPath() {
 }
 
 module.exports = {
-  // 发布文件的拷贝目录(默认产生一个随机目录，请手动指定，如path.resolve(__dirname, '../../your-build-repository')
-  path: '' || getRandomPath(),
+  // 发布文件的拷贝目录(默认产生一个随机目录，请手动指定，如path.resolve(__dirname, '../../your-build-repository')，支持数组形式来对多个目录进行处理
+  path: ['' || getRandomPath()],
   autoPush: false, // 是否自动push git仓库。若为true，则path需要为一个git仓库目录，程序将会自动拷贝文件后commit, push
 };
